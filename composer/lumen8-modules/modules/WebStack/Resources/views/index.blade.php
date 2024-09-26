49fa31d31d9ad77cf9bd597fc70c60a1b6c47c84
langnang <langnang.chen@outlook.com>
2024-09-26 10:25:06 +0800

    2024-09-26 10:25:03

--- composer/lumen8-modules/modules/WebStack/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('webstack::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('webstack.name') !!}
-    </p>
-@endsection
